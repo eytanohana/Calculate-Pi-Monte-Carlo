cf456c97fac87f1b2700d0b4a0b5d4889d166aaf
Eytan Ohana <eohana98@gmail.com>
2019-10-16 17:53:33 -0700

    Add title

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Scene.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Calculate Pi - Monte Carlo Simulation");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
